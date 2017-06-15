bf84912f48d2de747bf2d7fc9f7b66ad2a07ed2e
Lizzi Yin <lizziyin@gmail.com>
2017-06-15 15:40:32 +0800

    problem2 solution updated

--- Problems1to5.py
+++ Problems1to5.py
@@ -13,3 +13,18 @@ def problem1 ():
             divFiveSum += i
     result = divThreeSum + divFiveSum - divFifteenSum
     return (result)
+
+def problem2 ():
+    a = 1
+    b = 2
+    c = 3
+    fibsum = 0
+    while(a <= 4000000):
+        temp = b + c
+        a = b
+        b = c
+        c = temp
+        if (a % 2 == 0):
+            fibsum += a
+    return (fibsum)
+
